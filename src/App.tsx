3eb9e9b088aede8823ba248aa6f5573c87c154b8
Овчинников Алексей Евгеньевич <eastonsc6@yandex.ru>
2021-06-28 17:03:40 +0300

    add favicons and set title

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.scss';
 import Container from "./components/container";
 import store from "./store/store";
 import {Provider} from "mobx-react";
+import {Helmet} from "react-helmet";
 
 function App() {
     return (
         <Provider store={store}>
+            <Helmet>
+                <title>Погода</title>
+            </Helmet>
             <div className="App">
-                <header className="App-header">
-                    <Container/>
-                </header>
+                <Container/>
             </div>
         </Provider>
     );

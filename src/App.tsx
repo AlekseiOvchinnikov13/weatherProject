016db85ae5954691748b75b904b0255a300240a7
Овчинников Алексей Евгеньевич <eastonsc6@yandex.ru>
2021-06-28 17:18:08 +0300

    fix title

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import './App.scss';
 import Container from "./components/container";
 import store from "./store/store";
 import {Provider} from "mobx-react";
-import {Helmet} from "react-helmet";
 
 function App() {
     return (
         <Provider store={store}>
-            <Helmet>
-                <title>Погода</title>
-            </Helmet>
             <div className="App">
                 <Container/>
             </div>
